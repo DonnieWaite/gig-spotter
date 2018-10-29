eb77a55c7163421cdc7af3720fc035026d3cc786
Donnie Waite <donnie.e.waite@gmail.com>
2018-10-29 16:14:57 -0400

    made devise authentication and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  :recoverable, :rememberable, :validatable
+
+
+  validates :email, presence: true
+  validates :encrypted_password, presence: true
+  validates :birthday, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  
 end
