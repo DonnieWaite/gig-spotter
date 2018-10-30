93fea49bbbaa087eb3988144c5962f939adff241
Donnie Waite <donnie.e.waite@gmail.com>
2018-10-30 14:53:14 -0400

    made api end point and configured react

---
+++ app/javascript/react/components/SpotterIndex.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+
+class SpotterIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        hello
+      </div>
+    )
+  }
+}
+export default SpotterIndex;
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :bands, only: :index
+    end
+  end
+
   root 'homes#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
