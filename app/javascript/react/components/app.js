05b35e1dc97cb2e376c59313932191391181ea6b
Donnie Waite <donnie.e.waite@gmail.com>
2018-10-31 13:11:15 -0400

    made the show page for a band.

--- app/javascript/react/components/app.js
+++ app/javascript/react/components/app.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Router, browserHistory, Route, IndexRoute, Link } from 'react-router';
 import SpotterIndex from './SpotterIndex'
+import SpotterShowPage from './SpotterShowPage'
 
 export const App = (props) => {
   return (
   <Router history={browserHistory}>
     <Route path="/" component={SpotterIndex}/>
+    <Route path="/bands/:id" component={SpotterShowPage}/>
   </Router>
   )
 }
