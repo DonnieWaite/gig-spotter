dd5363763def1b066503aa261bf9897236202b3e
Donnie Waite <donnie.e.waite@gmail.com>
2018-11-01 14:13:50 -0400

    test for both api controllers

---
+++ spec/controllers/api/v1/bands_controller.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::BandsController, type: :controller do
+  describe 'a' do
+    let!(:user) {User.create(first_name: "Richard", last_name: "Smith", email: "RSmith@gmail.com", password: "password123", password_confirmation: "password123", birthday: Date.new(1995,2,3))}
+
+    before(:each) do
+      allow(controller).to receive(:current_user).and_return(user)
+
+      Band.create(band_name: "Very helpful!", band_bio: "Booking shows makes me happy", band_image: "https://www.ccreadingfarm.com/wp-content/uploads/doggy-day.jpg", bandcamp_url: "www.google.com")
+
+    end
+
+    describe "GET#index" do
+      it "should return a list of all the bands" do
+        get :index
+        returned_json = JSON.parse(response.body)
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq "application/json"
+        binding.pry
+        expect(returned_json.length).to eq 1
+        expect(returned_json["band_name"]).to eq "Very helpful!"
+      end
+    end
+
+
+  end
+end
---
+++ spec/controllers/api/v1/bookers_controller_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::BookersController, type: :controller do
+  describe 'a' do
+    let!(:user) {User.create(first_name: "Richard", last_name: "Smith", email: "RSmith@gmail.com", password: "password123", password_confirmation: "password123", birthday: Date.new(1995,2,3))}
+
+    before(:each) do
+      allow(controller).to receive(:current_user).and_return(user)
+
+      Booker.create(organization_name: "Very helpful!", booker_bio: "Booking shows makes me happy", booker_image: "https://www.ccreadingfarm.com/wp-content/uploads/doggy-day.jpg")
+
+    end
+
+    describe "GET#index" do
+      it "should return a list of all the bands" do
+        get :index
+        returned_json = JSON.parse(response.body)
+        expect(response.status).to eq 200
+        expect(response.content_type).to eq "application/json"
+        expect(returned_json.length).to eq 1
+        expect(returned_json[0]["organization_name"]).to eq "Very helpful!"
+      end
+    end
+
+
+  end
+end
