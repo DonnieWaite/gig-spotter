aeac16f77f3f20168224f829c34243973cf65fbf
Donnie Waite <donnie.e.waite@gmail.com>
2018-12-03 16:19:24 -0500

    fixed failing test

--- spec/controllers/api/v1/bookers_controller_spec.rb
+++ spec/controllers/api/v1/bookers_controller_spec.rb
@@ -12,13 +12,13 @@ RSpec.describe Api::V1::BookersController, type: :controller do
     end
 
     describe "GET#index" do
-      it "should return a list of all the bands" do
+      it "should return a list of all the bookers" do
         get :index
         returned_json = JSON.parse(response.body)
         expect(response.status).to eq 200
         expect(response.content_type).to eq "application/json"
         expect(returned_json.length).to eq 1
-        expect(returned_json[0]["organization_name"]).to eq "Very helpful!"
+        expect(returned_json["bookers"][0]["organization_name"]).to eq "Very helpful!"
       end
     end
 
